9d3310e42cc725dc71d7e5aeeb4e9062bc0731e1
Benjamin Harris <harben31@gmail.com>
2021-04-16 13:48:57 -0700

    reservations are getting stored

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const ResoClass = require('./assets/js/lib/resoClass');
 
 const path = require('path');
@@ -10,38 +11,43 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const tables = [
-    {
-        name: "Billy",
-        number: 5555555555,
-        id: 1234,
-        email: 'email@email.email'
-    },
-    {
-        name: "Willy",
-        number: 66666666666,
-        id: 4321,
-        email: 'dmail@dmail.dmail'
-    }
-]
+
 //when no path specified sends user to home page
 app.get('/', (req, res)=>res.sendFile(path.join(__dirname, './public/home.html')));
 
 //cannot find file even though it is there
-// app.get('/reso', (req, res)=>res.sendFile(path.join(__dirname, './public/reso.hmtl')));
+app.get('/reso', (req, res)=>res.sendFile(path.join(__dirname, './public/reso.hmtl')));
 
 //when client requests reservation data
-app.get('/api/tables', (req, res)=>res.json(tables));
+app.get('/api/tables', (req, res)=>{
+    fs.readFile('./db/db.json', 'utf8', (err, data)=>{
+        if(err) throw err;
+        res.send(data[1]);
+    })
+});
+// console.log(tables);
 
 //when client posts new reservation data 
 app.post('/api/reso', (req, res)=>{
-    const tablePost = req.body;
-    const newTable = new ResoClass(tablePost.name, tablePost.number, tablePost.id, tablePost.email );
-    tables.push(newTable);
-    res.send(`Thank you ${tablePost.name} for making a reservation.
-    We ill call you at ${tablePost.phone} or email you at ${tablePost.email} 
-    when your table is ready. 
-    Your reservation ID is ${tablePost.id}`)
+    
+    // const newTable = new ResoClass(tablePost.name, tablePost.number, tablePost.id, tablePost.email );
+    fs.readFile('./db/db.json', ((err, data)=>{
+        if (err) throw err;
+        const json = JSON.parse(data);
+        console.log(json);
+        const tablePost = req.body;
+        console.log(tablePost);
+        json.push(tablePost);
+        fs.writeFile('./db/db.json', JSON.stringify(json), (err, data)=>{
+            if(err) throw err;
+        })
+    }))
+    // tables.push(JSON.stringify(newTable));
+    // res.send(`Thank you ${tablePost.name} for making a reservation.
+    // We ill call you at ${tablePost.phone} or email you at ${tablePost.email} 
+    // when your table is ready. 
+    // Your reservation ID is ${tablePost.id}`)
+    res.end()
 });
 
 app.listen(port, ()=>console.log(`server runnin on ${port}`))
